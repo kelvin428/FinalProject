60a8685fc70e93ee13ba01c779375e03ea939998
kelvin428 <81065342+kelvin428@users.noreply.github.com>
2021-04-30 12:14:40 +0800

    Add files via upload

---
+++ booking.js
@@ -0,0 +1,24 @@
+const fname = document.getElementById('fname').value;
+const lname = document.getElementById('lname').value;
+const email = document.getElementById('email').value;
+const phone = document.getElementById('phone').value;
+const address = document.getElementById('address').value;
+const form = document.getElementById('form');
+const errorElement = document.getElementById('error');
+let message =[];
+
+
+
+form.addEventListener('submit',(e)=>{
+    if (phone<0){
+        message.push("The incorrect phone number.");
+
+    }
+        if(message.length>0){
+            e.preventDefault();
+            errorElement.innerText=message.join(',');
+            message = [];
+        }
+
+})
+
